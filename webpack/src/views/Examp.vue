416d83271fda3dd1ee7db0d8efbe56b307f2ab81
Johnny_Wick <wolfmib@gmail.com>
2020-03-11 17:56:26 +0800

    updated_git_hub_link_in_Examp_vue

--- webpack/src/views/Examp.vue
+++ webpack/src/views/Examp.vue
@@ -1,6 +1,8 @@
 <template>
   <div> 
-    <h1>This is an Exam{{my_message}} page 🦜... </h1>	
+   
+
+    <p>This is an Exam{{my_message}} page 🦜... <p>	
     <p> Just Type the examp/1, examp/2 for each questions ...✍️<p>
     <!--
     <p> Original ID {{ $route.params.id}} </p>
@@ -8,7 +10,24 @@
     <p> CurrentID   {{ current_id }} -->
     <hr>
     <div>
+
+    <b-container class="center">
+      <b-card
+      overlay
+      img-src="https://picsum.photos/600/300/?image=25"
+      img-alt="Card Image"
+      text-variant="black"
+      title="JA Examp Collections"
+      >
+      </b-card>
+
+      <b-button href="https://github.com/wolfmib/vuejs_with_examP_demo" variant="primary"> Source Code on Github</b-button>
+
+    </b-container>
+    <hr>
     <b-container fluid class="p-4 bg-dark">
+    
+
       <b-row>
         <b-col>
  <b-img alt="left image" fluid-grow :src="require(`../assets/examp_${my_message}.jpg`)"></b-img>
