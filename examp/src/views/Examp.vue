8aed97ef515ba7081b1eac618cbf6b6256eb9733
Johnny_Wick <wolfmib@gmail.com>
2020-03-12 12:01:29 +0800

    add_questions_to_11

--- examp/src/views/Examp.vue
+++ examp/src/views/Examp.vue
@@ -11,47 +11,56 @@
     <hr>
     <div>
 
-    <b-container class="center">
-      <b-card
-      overlay
-      img-src="https://picsum.photos/600/300/?image=25"
-      img-alt="Card Image"
-      text-variant="black"
-      title="JA Examp Collections"
-      >
-      </b-card>
-
-      <b-button href="https://github.com/wolfmib/vuejs_with_examP_demo" variant="primary"> Source Code on Github</b-button>
-
-    </b-container>
-    <hr>
-    <b-container fluid class="p-4 bg-dark">
-    
-
-      <b-row>
+      <!-- Main Photos  Here -->
+      <b-container class="center">
+        <b-row>
+        <b-col sm="auto"></b-col>
         <b-col>
- <b-img alt="left image" fluid-grow :src="require(`../assets/examp_${my_message}.jpg`)"></b-img>
+          <b-card
+          overlay
+          img-src="https://picsum.photos/600/300/?image=25"
+          img-alt="Card Image"
+          text-variant="black"
+          title="JA Examp Collections"
+          >
+          </b-card>
         </b-col>
-      </b-row>  
-      <b-row>
-        <hr>
-      </b-row>
-      <b-row>
-        <b-col>
-             <b-img alt="left image" fluid-grow :src="require(`../assets/result_${my_message}.jpg`)"></b-img>
-        </b-col>
-      </b-row>  
-    </b-container>
+        <b-col sm="auto"></b-col>
+        </b-row>
+        <b-row>
+          <b-col></b-col>
+          <b-col>
+               <b-button href="https://github.com/wolfmib/vuejs_with_examP_demo" variant="primary"> Source Code on Github</b-button>
+          </b-col>
+          <b-col></b-col>
+        </b-row>       
+      </b-container>
+      <hr>
+
+      <!-- Main Content Questions and Draft here-->
+      <b-container fluid class="p-4 bg-dark">
+        <b-row>
+          <b-col sm="auto"></b-col>
+          <b-col>
+             <b-tabs content-class="mt-3">
+              <b-tab title="Questions" active>
+                <b-img alt="left image" fluid-grow :src="require(`../assets/examp_${my_message}.jpg`)"></b-img>
+              </b-tab>
+              <b-tab title="Draft Solution">
+                  <b-img alt="left image" fluid-grow :src="require(`../assets/result_${my_message}.jpg`)"></b-img>
+              </b-tab>
+              <b-tab title="Disabled" disabled><p>I'm a disabled tab!</p></b-tab>
+            </b-tabs>    
+          </b-col>
+          <b-col sm="auto"></b-col>
+        </b-row>   
+      </b-container>
     </div>
-
   </div>
 </template>
+
+
 <script>
-
-
-
-
-
 export default {
   data: function(){
     return {
@@ -97,3 +106,8 @@ export default {
   }
 }
 </script>
+
+
+<style>
+
+</style>
