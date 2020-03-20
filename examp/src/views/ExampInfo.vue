127fc7669f4dcf3173ba5923f6332c805cd99a44
Johnny_Wick <wolfmib@gmail.com>
2020-03-20 16:42:08 +0800

    jason_finish_example_43

--- examp/src/views/ExampInfo.vue
+++ examp/src/views/ExampInfo.vue
@@ -1,13 +1,35 @@
 <template>
   <div>
-    <b-container>
-      <b-row left>
-        <b-button
-          variant="info"
-          href="https://drive.google.com/file/d/1WCmTcKr1dOGTerjTs4XAkMzuavPfxi2E/view?usp=sharing"
-          >Go TO Examp_42 Hint Guide</b-button
-        >
+    <hr />
+    <b-container fluid="sm">
+      <b-row>
+        <b-col>
+          <b-button
+            variant="info"
+            href="https://drive.google.com/file/d/1WCmTcKr1dOGTerjTs4XAkMzuavPfxi2E/view?usp=sharing"
+          >Go TO Examp_42 Hint Guide</b-button>
+        </b-col>
+        <b-col></b-col>
+        <b-col></b-col>
+      </b-row>
+      <hr />
+      <b-row>
+        <b-col>
+          <b-button
+            variant="info"
+            href="https://faculty.math.illinois.edu/~fengzhu2/Additional%20Remarks%20on%20ICE7.2.pdf"
+          >Go TO Examp_43 Web Soluction Link</b-button>
+        </b-col>
+        <b-col>
+          <b-button
+            variant="info"
+            href="https://www.statisticshowto.datasciencecentral.com/negative-binomial-experiment/"
+          >Go TO Examp_43 Negative Binomial Distribution</b-button>
+        </b-col>
+        <b-col></b-col>
       </b-row>
     </b-container>
   </div>
 </template>
+
+
